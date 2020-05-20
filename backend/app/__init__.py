42271c50d68e6b869899464c7ceeb5f1285a937e
Dmitry <dima123-99@mail.ru>
2020-05-20 18:18:35 +0700

    Refactoring app with Api by linters

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,22 +1,34 @@
+"""Settings"""
+
 import sys
 import os
 import logging
 
 from logging.handlers import RotatingFileHandler, SMTPHandler
 from flask import Flask, request
-from config import Configuration
+from config import BaseConfiguration
 
 # For visibility of folder
 sys.path.insert(0, os.path.join('./../algorithms'))
 
+
 def register_logger(app, logs_dir):
-    log_file_format = "[%(levelname)s] - %(asctime)s - %(name)s - : %(message)s in %(pathname)s:[%(funcName)s]:%(lineno)d"
+    """Setting up logger"""
+    log_file_format = """
+            [%(levelname)s] - %(asctime)s - %(name)s - : %(message)s in %(pathname)s:[%(funcName)s]:%(lineno)d
+    """
     log_console_format = "[%(levelname)s]: %(message)s"
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(logging.Formatter(log_console_format))
 
-    file_handler = RotatingFileHandler(os.path.join(logs_dir, 'api.log'), maxBytes=1e6, backupCount=10, encoding='utf-8')
+    file_handler = RotatingFileHandler(
+        os.path.join(
+            logs_dir,
+            'api.log'),
+        maxBytes=1e6,
+        backupCount=10,
+        encoding='utf-8')
     file_handler.setFormatter(logging.Formatter(log_file_format))
 
     app.logger.addHandler(file_handler)
@@ -25,7 +37,9 @@ def register_logger(app, logs_dir):
     log_werkzeug.addHandler(file_handler)
     log_werkzeug.addHandler(console_handler)
 
-def create_app(config_class=Configuration):
+
+def create_app(config_class=BaseConfiguration):
+    """Setting up app"""
     app = Flask(__name__)
     app.config.from_object(config_class)
 
@@ -37,7 +51,7 @@ def create_app(config_class=Configuration):
     logs_dir = os.path.join(base_dir, 'logs')
     if not os.path.exists(logs_dir):
         os.mkdir(logs_dir)
-    
+
     register_logger(app, logs_dir)
 
     app.logger.setLevel(logging.INFO)
