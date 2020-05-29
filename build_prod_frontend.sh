f5b7659587a6c101479861186099e697800e8a82
Dmitry <dima123-99@mail.ru>
2020-05-30 06:39:50 +0700

    Updated bash scripts

--- build_prod_frontend.sh
+++ build_prod_frontend.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+export REACT_APP_API_URL=https://krasnodar-road-network.ru
 cd ./frontend
 rm -r ./build
 npm run build
--- run_local_frontend.sh
+++ run_local_frontend.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-set -e
-set -u
+export REACT_APP_API_URL=localhost:5000
 cd frontend
 npm start
 cd .. 
